41b94c86033d1c3f8f6c6cd66af59cdc08008e14
pschwarz <schwarz@ihf.rwth-aachen.de>
2017-05-12 12:47:16 +0200

    Closes #4

--- app/src/main/java/de/hampager/dapnetmobile/fragments/CallFragment.java
+++ app/src/main/java/de/hampager/dapnetmobile/fragments/CallFragment.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -27,7 +28,11 @@ public class CallFragment extends Fragment {
     private final String TAG = "CallFragment";
     private RecyclerView recyclerView;
     private DataAdapter adapter;
-
+    private SwipeRefreshLayout mSwipe;
+    private String server;
+    private String user;
+    private String password;
+    private Boolean admin;
     public CallFragment() {
         // Required empty public constructor
     }
@@ -49,10 +54,10 @@ public class CallFragment extends Fragment {
         mLayoutManager.setStackFromEnd(true);
         recyclerView.setLayoutManager(mLayoutManager);
         SharedPreferences sharedPref = this.getActivity().getSharedPreferences("sharedPref", Context.MODE_PRIVATE);
-        String server = sharedPref.getString("server", null);
-        String user = sharedPref.getString("user", null);
-        String password = sharedPref.getString("pass", null);
-        Boolean admin = sharedPref.getBoolean("admin", true);
+        server = sharedPref.getString("server", null);
+        user = sharedPref.getString("user", null);
+        password = sharedPref.getString("pass", null);
+        admin = sharedPref.getBoolean("admin", true);
         fetchJSON(server, user, password, admin);
     }
 
@@ -77,6 +82,7 @@ public class CallFragment extends Fragment {
                     ArrayList<HamnetCall> data = response.body();
                     adapter = new DataAdapter(data);
                     recyclerView.setAdapter(adapter);
+                    mSwipe.setRefreshing(false);
                 } else {
                     //APIError error = ErrorUtils.parseError(response);
                     Log.e(TAG, "Error " + response.code());
@@ -106,6 +112,26 @@ public class CallFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_call, container, false);
         v.setTag(TAG);
         initViews(v);
+        mSwipe = (SwipeRefreshLayout) v.findViewById(R.id.swipeRefreshCalls);
+
+        // Setup refresh listener which triggers new data loading
+
+        mSwipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+            @Override
+
+            public void onRefresh() {
+
+                // Your code to refresh the list here.
+
+                // Make sure you call swipeContainer.setRefreshing(false)
+
+                // once the network request has completed successfully.
+                fetchJSON(server, user, password, admin);
+
+            }
+
+        });
         return v;
     }
 
